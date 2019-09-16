41787c4ab9052cf31f4f77fc27e6d6469bac4b6e
Damjan Georgievski <gdamjan@hfour.com>
2019-09-16 20:56:50 +0200

    use the same format for output as the input
    image::guess_format is the same thing that image::load_from_memory used
    to decode the image so it's fine to use it.

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,11 @@ pub fn normalize(bytes: &[u8]) -> Result<Box<[u8]>, JsValue> {
     let img = image::load_from_memory(&bytes)
         .map_err(|err| JsValue::from(format!("{}", err)))?;
 
+    let format = image::guess_format(&bytes)
+        .map_err(|err| JsValue::from(format!("{}", err)))?;
+
     let mut buf: Vec<u8> = Vec::new();
 
-    let format: image::ImageFormat = image::JPEG;
     let thumbnail = img.thumbnail(800, 800);
     thumbnail
         .write_to(&mut buf, format)
