a002ceaf194400f25b767324230c7084a5ee9ca5
Damjan Georgievski <gdamjan@hfour.com>
2019-09-16 20:20:08 +0200

    implement the normalize as img.thumbnail
    * the output format is fixed to JPEG for now
    * thumbnail docs:
      https://docs.rs/image/0.22.2/image/enum.DynamicImage.html#method.thumbnail

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
+use image;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(catch)]
 pub fn normalize(bytes: &[u8]) -> Result<Box<[u8]>, JsValue> {
-    Err(JsValue::from_str("simulated error"))
+    let img = image::load_from_memory(&bytes)
+        .map_err(|err| JsValue::from(format!("{}", err)))?;
+
+    let mut buf: Vec<u8> = Vec::new();
+
+    let format: image::ImageFormat = image::JPEG;
+    let thumbnail = img.thumbnail(800, 800);
+    thumbnail
+        .write_to(&mut buf, format)
+        .map_err(|err| JsValue::from(format!("{}", err)))?;
+
+    Ok(buf.into_boxed_slice())
 }
