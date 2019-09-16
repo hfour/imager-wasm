e0206ca38d29e3dded56646272c2963ffde52517
Damjan Georgievski <gdamjan@hfour.com>
2019-09-16 20:20:02 +0200

    and this is how we "throw" from rust

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,5 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(catch)]
 pub fn normalize(bytes: &[u8]) -> Result<Box<[u8]>, JsValue> {
-    Ok((0..42).collect::<Vec<u8>>().into_boxed_slice())
+    Err(JsValue::from_str("simulated error"))
 }
